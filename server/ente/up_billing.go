b29c5f095b4d8473a405928c5c41d1b9bacd9072
andrey <andrey@unplugged.com>
2025-08-05 17:27:35 +0300

    Update `WebhookRequest` to use `int64` for timestamps, reducing dependency on `time.Time` and streamlining data parsing

--- server/ente/up_billing.go
+++ server/ente/up_billing.go
@@ -1,21 +1,19 @@
 package ente
 
-import "time"
-
 const (
 	Unplugged PaymentProvider = "unplugged"
 )
 
 type WebhookRequest struct {
-	ID              string    `json:"id"`
-	Type            string    `json:"type"`
-	Interval        string    `json:"interval"`
-	Price           float64   `json:"price"`
-	Currency        string    `json:"currency"`
-	Provider        string    `json:"provider"`
-	NextChargeDate  time.Time `json:"nextChargeDate"`
-	ExpirationDate  time.Time `json:"expirationDate"`
-	ServerTimestamp time.Time `json:"serverTimestamp"`
-	Username        string    `json:"username"`
-	Event           string    `json:"event"`
+	ID              string  `json:"id"`
+	Type            string  `json:"type"`
+	Interval        string  `json:"interval"`
+	Price           float64 `json:"price"`
+	Currency        string  `json:"currency"`
+	Provider        string  `json:"provider"`
+	NextChargeDate  int64   `json:"nextChargeDate"`
+	ExpirationDate  int64   `json:"expirationDate"`
+	ServerTimestamp int64   `json:"serverTimestamp"`
+	Username        string  `json:"username"`
+	Event           string  `json:"event"`
 }
